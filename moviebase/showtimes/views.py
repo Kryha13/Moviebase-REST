488e3abc7c0ed28572dee385aa24aed5ec11c252
Krystian Baranowski <krystian.qaz123@gmail.com>
2019-03-23 15:24:28 +0100

    filtering by movie name screenings

--- moviebase/showtimes/views.py
+++ moviebase/showtimes/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
 
 from showtimes.models import Cinema, Screening
-from showtimes.serializers import CinemaSerializer
-from rest_framework import generics
+from showtimes.serializers import ScreeningSerializer, CinemaSerializer
+from rest_framework import generics, filters
 
 
 # Create your views here.
-
 
 class CinemaListView(generics.ListCreateAPIView):
     queryset = Cinema.objects.all()
@@ -16,3 +16,17 @@ class CinemaListView(generics.ListCreateAPIView):
 class CinemaView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cinema.objects.all()
     serializer_class = CinemaSerializer
+
+
+class ScreeningListView(generics.ListCreateAPIView):
+    queryset = Screening.objects.all()
+    serializer_class = ScreeningSerializer
+    filter_backends = (filters.SearchFilter,)  ###
+    search_fields = ('movie__title', )  ###
+
+
+class ScreeningView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Screening.objects.all()
+    serializer_class = ScreeningSerializer
+
+
