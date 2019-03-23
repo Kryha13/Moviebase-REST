b412b5cc731a228ed8a85a82fecbeda4a3d4a1d4
Krystian Baranowski <krystian.qaz123@gmail.com>
2019-03-23 15:16:32 +0100

    filter by city

--- moviebase/movielist/views.py
+++ moviebase/movielist/views.py
@@ -1,11 +1,13 @@
 from movielist.models import Movie
 from movielist.serializers import MovieSerializer
-from rest_framework import generics
+from rest_framework import generics, filters
 
 
 class MovieListView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    filter_backends = (filters.SearchFilter,)  ###
+    search_fields = ('cinema__city',)  ###
 
 
 class MovieView(generics.RetrieveUpdateDestroyAPIView):
